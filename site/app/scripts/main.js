be9ff3fd1d1efe6a9caa777196f1a7b6df49e758
Mateus Sobrinho <chocsx@hotmail.com>
2017-04-03 20:39:42 -0300

    home desktop ready

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	var logo = $('.logo img');
+	if($(window).width() < 480){
+		logo.attr("src", "./images/logo-small.png")
+	}
+	$('button.hamburger').click(function(){
+		$('nav ul').toggleClass('open');
+		$('.hamburger').toggleClass('is-active');
+	})
+	$('.content').slick({
+		arrows: false
+	});
+});
